c56d3f43307104e606c00fca61e068968df7dff6
happy_ryo <iwama.ryo.0731@gmail.com>
2023-05-30 18:13:57 +0900

    Change Params

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const postChat = async (
 
     const openai = new OpenAIApi(configuration);
     const response = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-4',
         messages: messages
     });
 
